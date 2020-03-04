49bd52d2ee3063af3574db1ba0cfb1e8ca7cfe26
DerekBoat <57238510+DerekBoat@users.noreply.github.com>
2020-03-04 14:34:39 -0600

    Update joins-solution.sql

--- joins-solution.sql
+++ joins-solution.sql
@@ -1,1 +1,13 @@
-Sql!
+  --Get all customers and their addresses.
+ SELECT * FROM "customers" JOIN "addresses" ON "customers"."id" = "addresses"."customer_id";
+ 
+ --Get all orders and their line items (orders, quantity and product)
+ SELECT * FROM "orders" JOIN "line_items" ON "orders"."id" = "line_items"."order_id" JOIN "products" ON "line_items"."product_id"="products"."id";
+ 
+ --Which warehouses have cheetos?
+SELECT * FROM "products" JOIN "warehouse_product" ON "products"."id" = "warehouse_product"."product_id" JOIN "warehouse" ON "warehouse_product"."warehouse_id"
+="warehouse"."id" WHERE "products"."description" = 'cheetos';
+ 
+ --Which warehouses have diet pepsi?
+ SELECT * FROM "products" JOIN "warehouse_product" ON "products"."id" = "warehouse_product"."product_id" JOIN "warehouse" ON "warehouse_product"."warehouse_id"
+="warehouse"."id" WHERE "products"."description" = 'diet pepsi';
